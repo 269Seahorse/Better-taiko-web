0d13f14f45492ba4491110aaa403f8056ca34a8d
LoveEevee <LoveEevee@users.noreply.github.com>
2020-03-07 05:39:48 +0300

    Add various tools

---
+++ tools/get_version.sh
@@ -0,0 +1,1 @@
+git log -1 --pretty="format:{\"commit\": \"%H\", \"commit_short\": \"%h\", \"version\": \"%ad\", \"url\": \"https://github.com/bui/taiko-web/\"}" --date="format:%y.%m.%d" > ../version.json
