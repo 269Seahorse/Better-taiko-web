96b1ba4b04478a14c42ff50bb8510ed63f83ff18
LoveEevee <LoveEevee@users.noreply.github.com>
2020-03-17 06:13:36 +0300

    Add git hooks to tools
    - Place these in your .git/hooks/ directory

--- tools/get_version.sh
+++ tools/get_version.sh
@@ -1,1 +1,3 @@
-git log -1 --pretty="format:{\"commit\": \"%H\", \"commit_short\": \"%h\", \"version\": \"%ad\", \"url\": \"https://github.com/bui/taiko-web/\"}" --date="format:%y.%m.%d" > ../version.json
+#!/bin/bash
+toplevel=$( git rev-parse --show-toplevel )
+git log -1 --pretty="format:{\"commit\": \"%H\", \"commit_short\": \"%h\", \"version\": \"%ad\"}" --date="format:%y.%m.%d" > "$toplevel/version.json"
